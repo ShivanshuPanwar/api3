5eb81ee445f0e2a1b7f4d86800e6be04592485e9
Shivanshu Panwar <118122773+ShivanshuPanwar@users.noreply.github.com>
2024-02-26 21:57:44 +0530

    Update employee.service.js

--- services/employee.service.js
+++ services/employee.service.js
@@ -3,22 +3,22 @@ const db = require('../db');
 module.exports.getAllEmployees = async () => {
     const [records] =  await db.query("SELECT * FROM employees");       
     return records;
-}
+};
 
 module.exports.getEmployeeById = async (id) => { 
     const [[record]] =  await db.query("SELECT * FROM employees WHERE id = ?", [id]);          
     return record;
-}
+};
 
 module.exports.deleteEmployee = async (id) => {
     const [{affectedRows}] =  await db.query("DELETE FROM employees WHERE id = ?", [id]); 
     return affectedRows;
-}
+};
 
 
 module.exports.postEmployee = async(obj, id=0) => {
     const [{affectedRows}] =  await db.query(`INSERT INTO employees SET ?`, obj);
-    if(id === 0){
+    if(id === 0) {
         return `A new employee with ID: ${affectedRows} has been created`;
     } else {
         return 'The data for the employee with ID: '+id+' has been updated';
